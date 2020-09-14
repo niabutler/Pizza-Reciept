8ec4d069fe759d98e1e565ca377dbfab078cc171
niabutler <niabutler@Nias-Air.smc.local>
2020-09-14 14:13:09 +1200

    End of sprint 5, calcuating the total price and printing the customer order as areceipt

--- main_file.py
+++ main_file.py
@@ -1,39 +1,59 @@
-"""Fruitbowl management programme."""
+"""Pizza Ordering programme."""
 from validations import get_validated_integer, get_validated_string, get_integer_conformation
 import math
 
 def starline():
-    print("*" * 50)
+    # print line of 50 stars
+    print("*" * 70)
 
 
 def dashline():
-    print("-" * 50)
+    # print line of 50 dashes
+    print("-" * 70)
 
 
 def dotline():
-    print("." * 50)
+    # print line of 50 dots
+    print("." * 70)
 
 
 def print_menu(m):
+    # print menu, formatted
     for i in range(0, len(m)):
         output = "{}: {}".format(m[i][0], m[i][1])
         print(output)
 
 
 def print_pizzas(p):
+    # print pizza menu with prices
     print("Pizza Menu")
     dotline()
     for i in range (0,len(p)):
+        # print price and name of pizzas
         output = ("${}0   {}".format(p[i][0], p[i][1]))
         print(output)
 
 
 def pizza_options(p):
+    # print pizza menu with indexes
     print("Pizza Menu")
     dotline()
+    # print headings above the information to make it easier to understand
+    titles = "{:5}   {:22} {:14}  {}".format("Item no.", "Name", "Description", "Price")
+    print(titles)
+    # print in order: index number +1, space, name, space, description and price
     for i in range(0, len(p)):
-        output = "{}:  ${}0 {} - {}".format(i+1, p[i][0], p[i][1], p[i][2])
+        output = "{:3}: {:5} {:20} {} -{:15} ${}0".format(i+1, "", p[i][1], "", p[i][2], p[i][0])
         print(output)
+
+def receipt_calc(co):
+    total_cost = 0
+    for i in range(0, len(co)):
+        sub_total = co[i][0] * co[i][1]
+        total_cost += sub_total
+    print("{:29}Total Cost: ${:5.2f} ".format("", total_cost))
+    print("{:36}GST: ${:5.2f} ".format("", total_cost*(3/23)))
+    dashline()
 
 
 def print_order(co):
@@ -41,9 +61,15 @@ def print_order(co):
         print("Your order is empty, start ordering!")
         return None
     print("Your order:")
+    dashline()
+    titles = "{:<5}  {:<20}  {:12} {}".format("Qty", "Flavour", "Price", "SUBTOTAL")
+    print(titles)
     for i in range(0, len(co)):
-        output = "x{} @${}0 {}".format(co[i][0], co[i][1], co[i][2])
+        sub_total = co[i][0] * co[i][1]
+        output = "x{:<5} {:<20} @ ${:<10.2f} ${:3.2f}".format(co[i][0], co[i][2], co[i][1], sub_total)
         print(output)
+    dashline()
+    receipt_calc(co)
 
 
 def add_to_order(p, co):
@@ -71,6 +97,21 @@ def add_to_order(p, co):
 
 
 def main_function():
+    """Run main menu.
+
+        Set pizza list, set customer order, set menu options, print menu.
+        Get user input, if {} letter is chosen, send to {} function.
+        Quit option to exit loop.
+        returns None
+        --------------
+        :return: None
+        --------------
+        L: list of lists, sub-list is [int, int, str]
+        --------------
+        Two dimensional list:
+        the sub-list has two items in it, [integer, string]
+        --------------
+        """
     starline()
     pizzas = [
         [18.50, "BBQ Chicken", "Description"],
@@ -81,20 +122,29 @@ def main_function():
     ]
 
     customer_order = []
+    customer_order_temp = [
+        [3, 18.5, "BBQ Chicken"],
+        [2, 18.5, "Cheese and Garlic"],
+        [1, 18.5, "Steak and Kumara"],
+
+    ]
 
     my_menu = [
         ("M", "See Menu"),
         ("A", "Add to order"),
         ("R", "Review Order"),
-        ("Q", "Quit")
+        ("Q", "Quit"),
+        ("T", "Test"),
         ]
     run = True
     while run is True:
         print("Main Menu")
         dotline()
+        # print main menu
         for i in range(0, len(my_menu)):
                 output = "{}: {}".format(my_menu[i][0], my_menu[i][1])
                 print(output)
+        # ask for user's choice as input
         choice = get_validated_string("Please choose an option: -> ", 1, 1).upper()
         if choice == "M":
             starline()
@@ -110,6 +160,9 @@ def main_function():
             starline()
             print_order(customer_order)
             starline()
+        elif choice =="T":
+            print("test")
+            print_order(customer_order_temp)
         else:
             print("Invalid entry, please try again.")
 
--- testing_file.py
+++ testing_file.py
@@ -12,6 +12,27 @@ def dashline():
 def dotline():
     print("." * 50)
 
+def get_integer_conformation(m, min, max):
+    run = True
+    while True:
+        try:
+            number = input(m)
+            number = number.replace(" ", "")
+            number = int(number)
+        except ValueError:
+            print("This is not a valid entry.")
+            continue
+        if number < min:
+            print("Your entry is too small")
+            continue
+        elif number > max:
+            print("Are you sure you want {} pizza's".format(number))
+            choice = get_validated_string("Press <enter> to confirm or any other key to re-enter", 0, 1).upper()
+            if choice == "":
+                return number
+        else:
+            return number
+
 
 def main_function():
     starline()
@@ -110,10 +131,89 @@ def add_to_order(p, co):
         else:
             print("Your input is invalid please choose y/n.")
 
+customer_order = [
+    [18.50, 3, "BBQ Chicken", "Description"],
+    [18.50, 3, "Hawaiian", "Description"],
+    [18.50, 1, "Cheese and Garlic", "Description"],
+    [25.50, 1, "Chicken and Brie", "Description"],
+    [25.50, 2, "Steak and Kumara", "Description"]
+]
+
+
+def receipt_calc(co):
+    total_cost = 0
+    for i in range(0, len(co)):
+        sum_row = co[i][0] * co[i][1]
+        total_cost += sum_row
+    print("${}".format(total_cost))
+
+
+def print_order(co):
+    if len(co) == 0:
+        print("Your order is empty, start ordering!")
+        return None
+    print("Your order:")
+    titles = "{:5}    {:10} {:15}".format("Qty", "Price", "Flavour")
+    print(titles)
+    dashline()
+    for i in range(0, len(co)):
+        output = "x{:<5} @ ${:<10.2f} {:15}".format(co[i][0], co[i][1], co[i][2])
+        print(output)
+
+def change_quantity():
+    co = [
+        [18.50, 3, "BBQ Chicken", "Description"],
+        [18.50, 3, "Hawaiian", "Description"],
+        [18.50, 1, "Cheese and Garlic", "Description"],
+        [25.50, 1, "Chicken and Brie", "Description"],
+        [25.50, 2, "Steak and Kumara", "Description"]
+    ]
+    index = get_validated_integer("Please enter the index number of the pizza you would like to change the quantity of: -> ", 1, len(co)-1)
+    print("Your currently have {} {} pizza/s in your order".format(co[index-1][1], co[index-1][2]))
+    new_quan = get_integer_conformation("Please enter your updated quantity of {} pizza's: -> ".format(co[index-1][2]), 1, 25)
+    print("Your order now has {} {} pizza's".format(new_quan, co[index-1][2]))
+    co[index - 1][2] = new_quan
+
+
+def update_menu(co):
+    my_menu = [
+        ("C", "Change a quantity"),
+        ("R", "Remove a Pizza"),
+        ("A", "Add a pizza"),
+        ("Q", "Main Menu")
+    ]
+
+    run = True
+    while run is True:
+        print("Review Order")
+        dotline()
+        print_order(co)
+        # print main menu
+        for i in range(0, len(my_menu)):
+            output = "{}: {}".format(my_menu[i][0], my_menu[i][1])
+            print(output)
+        # ask for user's choice as input
+        choice = get_validated_string("Please choose an option: -> ", 1, 1).upper()
+        if choice == "C":
+            starline()
+            starline()
+        elif choice == "R":
+            starline()
+        elif choice == "A":
+            starline()
+        elif choice == "Q":
+            starline()
+        else:
+            return None
+
+
 
 
 #print_pizzas(pizzas)
-main_function()
+#main_function()
 #pizza_options(pizzas)
 #add_to_order()
+#print_order(customer_order)
+#receipt_calc(customer_order)
+change_quantity()
 
